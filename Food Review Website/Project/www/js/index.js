b130a394bbbe11a987f881de66f17da0ac588135
M. SubhaN <naseem.subi1@outlook.com>
2023-10-11 13:48:15 -0400

    Initial Commit
    The first 4 project

---
+++ Food Review Website/Project/www/js/index.js
@@ -0,0 +1,6 @@
+ function clearForm() {
+            document.getElementById("first-name").value = "";
+            document.getElementById("last-name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("comment").value = "";
+        }
